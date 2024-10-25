e82368deedc53da1d94c3cf223f6ebfd72c7e4b8
dh-svg <ghifari251103@gmail.com>
2024-10-25 20:38:55 +0700

    Upload App Nilai

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -53,9 +54,10 @@
             <span class="float-end">
             versi 1.1 by <a href="http://dhani.kesug.com/?i=1" target="blank" rel="noopener noreferrer">Dhani Julfaindra</a>
             </span>
-        </div>
+            </div>
     </div>
 </div>
+
 
 <script src="http://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
